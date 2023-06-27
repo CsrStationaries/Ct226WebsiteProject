750887e3e453f2e8933f2d2f3ae30787e3e989ac
CsrStationaries <137565787+CsrStationaries@users.noreply.github.com>
2023-06-27 23:45:32 +0000

    Updated

--- project/stationaries/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Stationaries
-
-class StationariesAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'quantity')
-
-admin.site.register(Stationaries, StationariesAdmin)
--- project/stationaries/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Stationaries(models.Model):
-    name = models.CharField(max_length= 255)
-    price = models.FloatField()
-    quantity = models.IntegerField()
-    image = models.CharField(max_length=2083)
-
